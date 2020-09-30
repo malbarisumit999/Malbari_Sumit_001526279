63293e37f443d9548c323c75475013b1a50abfbd
malbarisumit999 <malbari.s@northeastern.edu>
2020-10-01 02:41:52 +0530

    Assignment1 upload

---
+++ Assignment1/src/Business/Person.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author sai
+ */
+public class Person {
+    
+    private String name;
+    private String geographicData;
+    private String dateOfBirth;
+    private long telephoneNumber;
+    private long faxNumber;
+    private String emailAddress;
+    private String socialSecurityNumber;
+    private String medicalRecordNumber;
+    private String healthPlanBeneficiaryNumber;
+    private long bankAccountNumber;
+    private String certificateOrLicenseNumber;
+    private String vehicleIdentifier;
+    private String deviceIdentifier;
+    private String linkedIn;
+    private String ipAddress;
+    private String biometricIdentifier;
+    private String uniqueIdentyingNumber;
+    private String imagePath;
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(long telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public long getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(long faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getHealthPlanBeneficiaryNumber() {
+        return healthPlanBeneficiaryNumber;
+    }
+
+    public void setHealthPlanBeneficiaryNumber(String healthPlanBeneficiaryNumber) {
+        this.healthPlanBeneficiaryNumber = healthPlanBeneficiaryNumber;
+    }
+
+    public long getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(long bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getCertificateOrLicenseNumber() {
+        return certificateOrLicenseNumber;
+    }
+
+    public void setCertificateOrLicenseNumber(String certificateOrLicenseNumber) {
+        this.certificateOrLicenseNumber = certificateOrLicenseNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(String biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+
+    public String getUniqueIdentyingNumber() {
+        return uniqueIdentyingNumber;
+    }
+
+    public void setUniqueIdentyingNumber(String uniqueIdentyingNumber) {
+        this.uniqueIdentyingNumber = uniqueIdentyingNumber;
+    }
+    
+    
+    
+}
