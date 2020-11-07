e07c3d0f67aab37196f1b519a4e955e7795ce3a5
malbarisumit999 <malbari.s@northeastern.edu>
2020-11-06 23:16:00 -0500

    Manage Customer

--- FoodDelivery/src/Business/Customer/Customer.java
+++ FoodDelivery/src/Business/Customer/Customer.java
@@ -11,4 +11,33 @@ package Business.Customer;
  */
 public class Customer {
     
+    
+    private String name;
+    private String userName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    
+    public Customer(String userName){
+        this.userName = userName;
+    }
+    
+    
+    
+    
+    
 }
--- FoodDelivery/src/Business/Customer/CustomerDirectory.java
+++ FoodDelivery/src/Business/Customer/CustomerDirectory.java
@@ -5,10 +5,42 @@
  */
 package Business.Customer;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author harold
  */
 public class CustomerDirectory {
+    
+    private ArrayList<Customer> customerDirectory;
+    private Customer customer;
+    
+    public CustomerDirectory(){
+        this.customerDirectory = new ArrayList<Customer>();
+    }
+
+    public ArrayList<Customer> getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(ArrayList<Customer> customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+    
+    public Customer createUserAccount(String username){
+        customer = new Customer(username);
+        customerDirectory.add(customer);
+        return customer;
+    }
+    
+    
+    public void deleteCustomer(String username){
+        for(int i=0;i<customerDirectory.size();i++){
+            if(customerDirectory.get(i).getUserName()==username){
+                customerDirectory.remove(i);
+            }
+        }
+    }
     
 }
